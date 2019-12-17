e7a92b29a307ee7ab5462305cd0d51abb47933f4
gianmor <gmor97@gmail.com>
2019-12-17 15:20:16 +0100

    add information

---
+++ app/information/credits/credits-page.js
@@ -0,0 +1,9 @@
+
+const SearchViewModel = require("./credits-view-model");
+
+function onNavigatingTo(args) {
+    const component = args.object;
+    component.bindingContext = new SearchViewModel();
+}
+
+exports.onNavigatingTo = onNavigatingTo;
