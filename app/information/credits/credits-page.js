b4e11345489090bb55fc9df3439abf4e673fb25c
gianmor <gmor97@gmail.com>
2019-12-17 15:27:14 +0100

    delete credits, it's in branch tns-studio

--- app/information/credits/credits-page.js
+++
@@ -1,9 +0,0 @@
-
-const SearchViewModel = require("./credits-view-model");
-
-function onNavigatingTo(args) {
-    const component = args.object;
-    component.bindingContext = new SearchViewModel();
-}
-
-exports.onNavigatingTo = onNavigatingTo;
